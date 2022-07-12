6dc740725ad7b6c7b2213f9acd4ae6ae128f5aca
JamieClipperton <babygatter@yahoo.com>
2022-07-12 18:32:53 -0500

    updating

--- static/js/logic.js
+++ static/js/logic.js
@@ -7,16 +7,42 @@ d3.json(queryUrl).then(function (data) {
 
 function createFeatures(earthquakeData) {
 
-  // Define a function that we want to run once for each feature in the features array.
-  // Give each feature a popup that describes the place and time of the earthquake.
-  function onEachFeature(feature, layer) {
-    layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
+  function onEachFeature(features, layer) {
+    layer.bindPopup(`<h3>${features.properties.place}</h3><hr><p>${new Date(features.properties.time)}</p>`);
   }
 
-  // Create a GeoJSON layer that contains the features array on the earthquakeData object.
-  // Run the onEachFeature function once for each piece of data in the array.
-  var earthquakes = L.geoJSON(earthquakeData, {
-    onEachFeature: onEachFeature
+  function changeColor(features) {
+    if (features.properties.mag > 7)
+    return 'blue'
+    else if (features.properties.mag > 5)
+    return 'orange'
+    else if (features.properties.mag > 2.5)
+    return 'purple'
+    else 
+    return 'red'
+  };
+
+  function changeSize(features) {
+    if (features.geometry.coordinates[2] > 100)
+    return 0
+    else return features.geometry.coordinates[2]
+  };
+
+  let earthquakes = L.geoJSON(earthquakeData, {
+    onEachFeature: onEachFeature,
+    pointToLayer: function(features, latlng) {
+        return L.circleMarker(latlng)
+    },
+    style: function geojsonMarkerOptions(features) {
+        return {
+            radius: changeSize(features),
+            fillColor: changeColor(features),
+            color: '#000',
+            weight: 1,
+            opacity: 1,
+            fillOpacity: 0.8
+        }
+    }
   });
 
   // Send our earthquakes layer to the createMap function/
@@ -61,4 +87,32 @@ function createMap(earthquakes) {
     collapsed: false
   }).addTo(myMap);
 
+  // Legend
+  let legend = L.control({position: 'bottomright'});
+  
+  legend.onAdd = function(myMap) {
+
+    let div = L.DomUtil.create('div', 'info legend'),
+        grades = [0, 2.5, 5, 7],
+        colors = ['red', 'purple', 'orange', 'blue'];
+
+    for (let i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+        '<i style="background:' + colors[i] + '"></i> ' + 
+        grades[i] + (grades[i+1] ? `&ndash;` + grades[i+1] +'<br>' : '+');
+    }
+
+    return div;
+  };
+
+  legend.addTo(myMap);
+
+  d3.json('https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json').then(function (data) {
+
+    L.geoJSON(data, {
+
+    }).addTo(tectonicplates);
+
+  tectonicplates.addTo(myMap)
+  });
 }
