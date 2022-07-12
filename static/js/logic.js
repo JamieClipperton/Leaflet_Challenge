578d38a9e2a08359b855e1945a2154562e2b146b
JamieClipperton <babygatter@yahoo.com>
2022-07-12 17:52:46 -0500

    updatefiles

---
+++ static/js/logic.js
@@ -0,0 +1,6 @@
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+d3.json(queryUrl).then(function (data) {
+    console.log(data)
+    createFeatures(data.features);
+});
